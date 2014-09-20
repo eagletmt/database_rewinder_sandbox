fe9a573633a8fa254f6ec24c736fd216401d6367
Kohei Suzuki <kohei-suzuki@cookpad.com>
2014-09-20 10:19:13 +0900

    Reproduce bug

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'case 1' do
+    Article.create!
+  end
+
+  it 'case 2' do
+    p Article.count
+    Article.create!
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'case 1' do
+    Comment.create!
+  end
+
+  it 'case 2' do
+    p Comment.count
+    Comment.create!
+  end
 end
